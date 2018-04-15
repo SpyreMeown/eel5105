c985ef3702e6ee86cbaff0be48906e1b5464b6ab
Leonardo Held <leo_held@yahoo.com.br>
2018-04-14 21:23:58 -0300

    Update fulladder.vhd

--- LAB3/fulladder.vhd
+++ LAB3/fulladder.vhd
@@ -1,21 +1,22 @@
-library ieee;
-use ieee.Std_logic_1164.all;
+
+library IEEE;
+use IEEE.Std_logic_1164.all;
+
+--------------------------------------------------------------
 
 entity fulladder is
-port ( A 	: in std_logic;
-		 B 	: in std_logic;
-		 Cin	: in std_logic;
-		 S 	: out std_logic;
-		 Cout	: out std_logic
-		 );
+port (A: in std_logic;
+		  B: in std_logic;
+		  Cin: in std_logic;
+		  Cout: out std_logic;
+		  S: out std_logic
+		);
 end fulladder;
 
-architecture bhvr_fulladder of fulladder is
-	signal D, E, F : std_logic;
-begin
-	D		<= A xor B;
-	E		<= D and Cin;
-	F 		<= A and B;
-	S 		<= D xor Cin;
-	Cout 	<= E or F;
-end bhvr_fulladder;	
+--------------------------------------------------------------
+
+architecture circuito of fulladder is
+	begin
+		S <= A xor B xor Cin;
+		Cout <= (A and B) or (A and Cin) or (B and Cin);
+		end circuito;
